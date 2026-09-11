29c937dbe87da6839a55eae7631bff96c996888a
qixm <qxian.li@outlook.com>
2026-09-11 12:03:40 +0800

    Add name greeting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, Git!" << std::endl;
+    std::string name;
+    std::cout << "What's your name? ";
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!" << std::endl;
     return 0;
 }
-
